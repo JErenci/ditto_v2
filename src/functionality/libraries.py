3335a58f0de8ddeb017fe88cb6587b879178c9a4
JErenci <joan.guerrero.erencia@gmail.com>
2025-01-12 13:46:13 +0100

    Add install_and_import function to manage package installations dynamically and refactor

---
+++ src/functionality/libraries.py
@@ -0,0 +1,11 @@
+import subprocess
+import sys
+
+def install_and_import(package):
+    try:
+        __import__(package)
+    except ImportError:
+        print(f"{package} not found. Installing...")
+        subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+    finally:
+        globals()[package] = __import__(package)
